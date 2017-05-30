51c14deb76103735c9350eb2ed306425e446af64
Thomas Reynders <thomas.reynders@topic.nl>
2017-05-30 13:30:29 +0200

    Fix(term): Return promises instead of using callbacks

--- test/term.spec.js
+++ test/term.spec.js
@@ -9,6 +9,7 @@ const fs = require('fs'),
   mockfs = require('mock-fs'),
   path = require('path'),
   sinon = require('sinon'),
+  sinonStubPromise = require('sinon-stub-promise'),
   should = require('should'),
   term = require('../lib/term');
 
@@ -18,10 +19,15 @@ describe('Testing term Function', () => {
   const rootPath = '/dev';
   const tty0 = 'tty0';
 
+
+  before(() => {
+    sinonStubPromise(sinon);
+  });
+
   beforeEach(() => {
     mockfs({
       [rootPath]: {
-        [tty0]: 'output'
+        [tty0]: Buffer.from('output')
       }
     })
   });
@@ -34,32 +40,29 @@ describe('Testing term Function', () => {
     mockfs.restore();
   });
 
-  it('should return a events.EventEmitter() object', (done) => {
+  it('should return a events.EventEmitter() object', () => {
 
     const ttyPath = path.resolve(rootPath, tty0),
-      delimiter = '\r\n',
-      callback = function (event) {
+      delimiter = '\r\n';
+
+    fs.open(ttyPath, 'r+', async function(error, fd){
+      if (error) {
+        should.fail(error);
+      }
+
+      try {
+        const event = await term(ttyPath, delimiter, fd);
+
         should.exist(event);
         event.should.be.an.instanceOf(events.EventEmitter);
         event.should.have.property('serialPort', ttyPath);
         event.should.have.property('send').and.be.an.instanceOf(Function);
         event.should.have.property('close').and.be.an.instanceOf(Function);
-        done();
-      };
 
-    const stub = sandbox.stub(tty, 'ReadStream').callsFake(function() {
-      // Stub the tty readstream
-      return fs.createReadStream(ttyPath);
-    });
-
-    fs.open(ttyPath, 'r+', function(error, fd){
-      if (error) {
+      } catch (error) {
         should.fail(error);
       }
-
-      term(ttyPath, delimiter, callback, fd);
     });
   });
-
 
 });
